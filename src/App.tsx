f5aa5f86f97080e60d7b12dc8616613fc8cbae8d
tezzzar <olexandr.borodavko@gmail.com>
2022-11-01 01:49:08 +0200

    add list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
+// const title = <h1>Hello React.js</h1>
+const title = React.createElement('h1', null, 'React')
+
+const list = (
+    <ul>
+        <li>list item 1</li>
+        <li>list item 2</li>
+        <li>list item 3</li>
+    </ul>
+)
+
 function App() {
-    return <div className="App"></div>
+    return <div className="App">{title}</div>
 }
 export default App
